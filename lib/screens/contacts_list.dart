48ccf3a166f1d8a490ffd576a9d53433fd61888b
Bifi92 <bifi@pop-os.localdomain>
2022-07-17 12:47:17 -0300

    first commit

---
+++ lib/screens/contacts_list.dart
@@ -0,0 +1,83 @@
+import 'package:bytebank2/database/dao/contact_dao.dart';
+import 'package:bytebank2/models/contact.dart';
+import 'package:bytebank2/screens/contact_form.dart';
+import 'package:flutter/material.dart';
+
+class ContactsList extends StatelessWidget {
+  final ContactDao _dao = ContactDao();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contacts'),
+        backgroundColor: Colors.green[900],
+      ),
+      body: FutureBuilder<List<Contact>>(
+        initialData: const [],
+        future: _dao.findAll(),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              break;
+            case ConnectionState.waiting:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: const [
+                    CircularProgressIndicator(),
+                    Text('Loading'),
+                  ],
+                ),
+              );
+            case ConnectionState.active:
+              break;
+            case ConnectionState.done:
+              final List<Contact> contacts = snapshot.data as List<Contact>;
+              return ListView.builder(
+                itemBuilder: (context, index) {
+                  final Contact contact = contacts[index];
+                  return _ContactItem(contact);
+                },
+                itemCount: contacts.length,
+              );
+          }
+          return const Text('Unknown error!');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => const ContactForm(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _ContactItem extends StatelessWidget {
+  final Contact contact;
+
+  const _ContactItem(this.contact);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: ListTile(
+        title: Text(
+          contact.name,
+          style: const TextStyle(fontSize: 24.0),
+        ),
+        subtitle: Text(
+          contact.accountNumber.toString(),
+          style: const TextStyle(fontSize: 16.0),
+        ),
+      ),
+    );
+  }
+}
